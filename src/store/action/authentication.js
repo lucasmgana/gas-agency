1921e3c49f73cbc2e56a334cbe006afebbb32677
dele361 <issaabdelemani@gmail.com>
2022-07-10 05:36:40 -0700

    ade

--- src/store/action/authentication.js
+++ src/store/action/authentication.js
@@ -1,10 +1,13 @@
 /* eslint-disable prettier/prettier */
+import firestore from '@react-native-firebase/firestore';
 
 export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
 export const LOGIN_FAILED = 'LOGIN_FAILED';
 export const LOGIN_LOADING = 'LOGIN_LOADING';
 
 export const login = () => {
+  const ref = firestore().collection('gaseagent');
+  console.log('hhhhhhhhhhhhhhhhhh',ref)
   return dispatch => {
     // dispatch({
     //   type: LOGIN_LOADING,
